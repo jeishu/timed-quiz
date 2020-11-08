a5b9af388f58e5629261509c5794a17a0797592b
jeishu <jeremy.zhu@utexas.edu>
2020-11-07 20:24:17 -0600

    Massive update: changed from jQuery to Javascript, randomized my questions again, added a correct/incorrect feature, saves the time left as score, time will subtract if incorrect as well

---
+++ assets/js/jQuery.js
@@ -0,0 +1,262 @@
+// DOM Elements
+    // Pages
+    let homePageEl = $(".homePage");
+    let rulesPageEl = $(".rulesPage");
+    let lbPageEl = $(".lbPage");
+    let quizPageEl = $(".quizPage");
+    let formPageEl = $(".formPage");
+    
+    // Home Page Buttons
+    let homeStartBtnEl = $(".homeStartBtn"); // goes to the rules page
+    let homeLbBtnEl = $(".homeLbBtn"); // goes to the leaderboard page
+    
+    // Rules Page Buttons
+    let rulesHomeBtnEl = $(".rulesHomeBtn"); // goes to the home page
+    let rulesStartBtnEl = $(".rulesStartBtn"); // goes to the quiz page
+
+    // Leaderboard Page
+    let lbHomeBtnEl = $(".lbHomeBtn"); // goes to the home page
+
+    // Timer Variable
+    let timerEl = $(".timer");
+    let secondsEl = $("#seconds"); // for some reason $("#seconds") was not working. 
+    
+    // Others
+    let questionEl = $(".question")
+    let answerWrapperEl = $(".answerWrapper");
+
+    // Quiz Page Buttons
+    let choicesEl = $(".choices");
+    let choiceAbtnEl = $("#choiceAbtn");
+    let choiceBbtnEl = $("#choiceBbtn");
+    let choiceCbtnEl = $("#choiceCbtn");
+    let choiceDbtnEl = $("#choiceDbtn");
+
+    // Form Page Buttons
+    let formHomeBtnEl = $(".formHomeBtn");
+    let formLbBtnEl = $(".formLbBtn");
+    let formScoreEl = $(".formScore");
+    let formSumbitBtnEl = document.getElementsByClassName(".formSumbitBtn");
+
+// score of the user 
+let scoreArr = []
+
+// Home Page Buttons
+    // if the 'start quiz' button is clicked, the rules page is shown and hides other pages
+    homeStartBtnEl.on("click", function() {
+        let rulesHide = true;
+
+        if (rulesHide === true) {
+            homePageEl.hide();
+            rulesPageEl.show();
+            lbPageEl.hide();
+            quizPageEl.hide();
+        }
+    });
+    // if the 'leaderboard' button is clicked, the leaderboard page is shown and hides other pages
+    homeLbBtnEl.on("click", function() {
+        let lbHide = true;
+
+        if (lbHide === true) {
+            homePageEl.hide();
+            rulesPageEl.hide();
+            lbPageEl.show();
+            quizPageEl.hide();
+        }
+    });
+
+// Rules Page Buttons
+    // if the 'back' button is clicked, the home page is shown and hides other pages
+    rulesHomeBtnEl.on("click", function() {
+        let homeHide = true;
+
+        if (homeHide === true) {
+            homePageEl.show();
+            rulesPageEl.hide();
+            lbPageEl.hide();
+            quizPageEl.hide();
+        }
+    });
+    // if the 'start quiz' button is clicked, the quiz page is shown and hides other pages
+    rulesStartBtnEl.on("click", function() {
+        timeStart();
+
+        let quizHide = true;
+
+        if (quizHide === true) {
+            homePageEl.hide();
+            rulesPageEl.hide();
+            lbPageEl.hide();
+            quizPageEl.show();
+        }
+        questionChange();
+    });
+
+// Leaderboard Page Button
+    // if back button is clicked, the home page is shown and hides other pages
+    lbHomeBtnEl.on("click", function() {
+        let homeHide = true;
+
+        if (homeHide === true) {
+            homePageEl.show();
+            rulesPageEl.hide();
+            lbPageEl.hide();
+            quizPageEl.hide();
+        }
+    });
+
+// Form Page Button
+    // if home page button is clicked, the home page is shown and hides other pages
+    formHomeBtnEl.on("click", function() {
+        let formHomeHide = true;
+
+        if (formHomeHide === true) {
+            homePageEl.show();
+            rulesPageEl.hide();
+            lbPageEl.hide();
+            quizPageEl.hide();
+            formPageEl.hide();
+        }
+        // reloads the page so the quiz counter and quizzes are reset
+        location.reload();
+    });
+    // if leaderboards button is clicked, the home page is shown and hides other pages
+    formLbBtnEl.on("click", function() {
+        let formLbHide = true;
+
+        if (formLbHide === true) {
+            homePageEl.hide();
+            rulesPageEl.hide();
+            lbPageEl.show();
+            quizPageEl.hide();
+            formPageEl.hide();
+        }
+        // reloads the page so the quiz counter and quizzes are reset
+        location.reload();
+    });
+
+// Array of Questions and Answers
+let questions = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+        choiceA1: "<script>",
+        choiceB1: "<scripting>",
+        choiceC1: "<js>",
+        choiceD1: "<javascript>",
+        answer:   "<script>"
+    },
+    {
+        question: "How can you add a comment in a JavaScript?",
+        choiceA1: "<!--This is a comment-->",
+        choiceB1: "'No this is a comment",
+        choiceC1: "// No, no. This is a comment",
+        choiceD1: " ~ Fools, I am the comment. ~",
+        answer:   "// No, no. This is a comment"
+    },
+    {
+        question: "How do you call a function named: justFunction?",
+        choiceA1: "call justFunction",
+        choiceB1: "function justFunction()",
+        choiceC1: "justFunction() - Hello?",
+        choiceD1: "justFunction()",
+        answer:   "justFunction()"
+    },
+    {
+        question: "How do you write a if statement?",
+        choiceA1: "If roses are red and violets are blue, why isn't my coding working?",
+        choiceB1: "if (violet != blue) {}",
+        choiceC1: "if [blue == color] {}",
+        choiceD1: "if {statement} ()",
+        answer:   "if (!violet === blue) {}"
+    },
+    {
+        question: "Which of these is an array?",
+        choiceA1: "var color = ['I', 'am', 'not', 'an', 'array'];",
+        choiceB1: "let color = ('I', 'am', 'an', 'array');",
+        choiceC1: "const color = {'I', 'am', 'totally', 'an', 'array'};",
+        choiceD1: "array color = ['I', 'am', 'definitely', 'not', 'an', 'array'];",
+        answer:   "var color = ['I', 'am', 'not', 'an', 'array'];"
+    }
+];
+
+// Starts Time
+let questionCounter = 0;
+let displayTime = "";
+
+answerWrapperEl.on("click", answerCheck);
+
+// Changes the questions and answers randomly from the array
+function questionChange() {
+    questionCounter++;
+    questionEl.text(questions[questionCounter].question);
+    choiceAbtnEl.text(questions[questionCounter].choiceA1);
+    choiceBbtnEl.text(questions[questionCounter].choiceB1);
+    choiceCbtnEl.text(questions[questionCounter].choiceC1);
+    choiceDbtnEl.text(questions[questionCounter].choiceD1);
+}
+
+function timeStart() {
+    
+    displayTime = 90;
+    questionEl.text(questions[questionCounter].question);
+    choiceAbtnEl.text(questions[questionCounter].choiceA1);
+    choiceBbtnEl.text(questions[questionCounter].choiceB1);
+    choiceCbtnEl.text(questions[questionCounter].choiceC1);
+    choiceDbtnEl.text(questions[questionCounter].choiceD1);
+
+    let timerInterval = setInterval(function() {
+        displayTime--;
+        secondsEl.text(displayTime);    
+
+        // if timer hits zero, it will go to the form page
+        if(displayTime === 0) {
+            clearInterval(timerInterval);
+            // console.log("This works");
+            quizPageEl.hide();
+            formPageEl.show();
+            formScoreEl.text(displayTime);
+        }
+        // if all questions are answered before the timer runs out, it will got to the form page
+        else if (questionCounter > questions.length-1) {
+            clearInterval(timerInterval);
+            quizPageEl.hide();
+            setTimeout(function(){
+                formPageEl.show();
+                formScoreEl.text(displayTime);
+            },500);
+        }
+    }, 1000);
+}
+
+let kaomoji = "";
+
+function answerCheck(e) {
+    let userChoice = e.target.id;
+    if (userChoice == "choiceAbtn" || userChoice == "choiceBbtn" || userChoice == "choiceCbtn" || userChoice == "choiceDbtn") {
+        let choiceText = document.getElementById(userChoice).innerText;
+        console.log(choiceText);
+        let answerChoice = (choiceText === questions[questionCounter].answer);
+        if (answerChoice) {
+            kaomoji = document.createElement("p");
+            kaomoji.textContent = "	ヽ(*⌒▽⌒*)ﾉ";
+            quizPageEl.append(kaomoji); // how to remove?
+        }
+        else {
+            kaomoji = document.createElement("p");
+            kaomoji.textContent = "	ヽ(　￣д￣)ノ";
+            quizPageEl.append(kaomoji); // how to remove?
+            displayTime.textContent = displayTime.textContent - 10; // Issue, not working..?
+        }
+        questionChange();
+    }
+}
+
+// storing scores
+function scoreKeep() {
+
+}
+
+formSumbitBtnEl.submit(function(event) {
+    event.preventDefault();
+    
+})
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,244 +1,281 @@
 // DOM Elements
     // Pages
-    let homePageEl = $(".homePage");
-    let rulesPageEl = $(".rulesPage");
-    let lbPageEl = $(".lbPage");
-    let quizPageEl = $(".quizPage");
-    let formPageEl = $(".formPage");
+    let homePageEl = document.querySelector(".homePage");
+    let rulesPageEl = document.querySelector(".rulesPage");
+    let lbPageEl = document.querySelector(".lbPage");
+    let quizPageEl = document.querySelector(".quizPage");
+    let formPageEl = document.querySelector(".formPage");
     
     // Home Page Buttons
-    let homeStartBtnEl = $(".homeStartBtn"); // goes to the rules page
-    let homeLbBtnEl = $(".homeLbBtn"); // goes to the leaderboard page
+    let homeStartBtnEl = document.querySelector(".homeStartBtn"); // goes to the rules page
+    let homeLbBtnEl = document.querySelector(".homeLbBtn"); // goes to the leaderboard page
     
     // Rules Page Buttons
-    let rulesHomeBtnEl = $(".rulesHomeBtn"); // goes to the home page
-    let rulesStartBtnEl = $(".rulesStartBtn"); // goes to the quiz page
+    let rulesHomeBtnEl = document.querySelector(".rulesHomeBtn"); // goes to the home page
+    let rulesStartBtnEl = document.querySelector(".rulesStartBtn"); // goes to the quiz page
 
     // Leaderboard Page
-    let lbHomeBtnEl = $(".lbHomeBtn"); // goes to the home page
+    let lbHomeBtnEl = document.querySelector(".lbHomeBtn"); // goes to the home page
 
     // Timer Variable
-    let timerEl = $(".timer");
-    let secondsEl = $("#seconds"); // for some reason $("#seconds") was not working. 
+    let timerEl = document.querySelector(".timer");
+    let secondsEl = document.querySelector("#seconds"); // for some reason document.querySelector("#seconds") was not working. 
     
     // Others
-    let questionEl = $(".question")
-    let answerWrapperEl = $(".answerWrapper");
+    let questionEl = document.querySelector("#question");
+    let answerWrapperEl = document.querySelector(".answerWrapper");
 
     // Quiz Page Buttons
-    let choicesEl = $(".choices");
-    let choiceAbtnEl = $("#choiceAbtn");
-    let choiceBbtnEl = $("#choiceBbtn");
-    let choiceCbtnEl = $("#choiceCbtn");
-    let choiceDbtnEl = $("#choiceDbtn");
+    let choicesEl = Array.from(document.getElementsByClassName("choices"));
+        // console.log(choicesEl);
+    let choiceAbtnEl = document.querySelector("#choice1btn");
+    let choiceBbtnEl = document.querySelector("#choice2btn");
+    let choiceCbtnEl = document.querySelector("#choice3btn");
+    let choiceDbtnEl = document.querySelector("#choice4btn");
 
     // Form Page Buttons
-    let formHomeBtnEl = $(".formHomeBtn");
-    let formLbBtnEl = $(".formLbBtn");
-    let formScoreEl = $(".formScore");
+    let formHomeBtnEl = document.querySelector(".formHomeBtn");
+    let formLbBtnEl = document.querySelector(".formLbBtn");
+    let formScoreEl = document.querySelector(".formScore");
+    let formSumbitBtnEl = document.querySelector(".formSumbitBtn");
+
+// score of the user 
+let scoreArr = [];
 
 // Home Page Buttons
     // if the 'start quiz' button is clicked, the rules page is shown and hides other pages
-    homeStartBtnEl.on("click", function() {
+    homeStartBtnEl.addEventListener("click", function() {
         let rulesHide = true;
 
         if (rulesHide === true) {
-            homePageEl.hide();
-            rulesPageEl.show();
-            lbPageEl.hide();
-            quizPageEl.hide();
+            homePageEl.style.display = "none";
+            rulesPageEl.style.display = "block";
+            lbPageEl.style.display = "none";
+            quizPageEl.style.display = "none";
         }
     });
     // if the 'leaderboard' button is clicked, the leaderboard page is shown and hides other pages
-    homeLbBtnEl.on("click", function() {
+    homeLbBtnEl.addEventListener("click", function() {
         let lbHide = true;
 
         if (lbHide === true) {
-            homePageEl.hide();
-            rulesPageEl.hide();
-            lbPageEl.show();
-            quizPageEl.hide();
+            homePageEl.style.display = "none";
+            rulesPageEl.style.display = "none";
+            lbPageEl.style.display = "block";
+            quizPageEl.style.display = "none";
         }
     });
 
 // Rules Page Buttons
     // if the 'back' button is clicked, the home page is shown and hides other pages
-    rulesHomeBtnEl.on("click", function() {
+    rulesHomeBtnEl.addEventListener("click", function() {
         let homeHide = true;
 
         if (homeHide === true) {
-            homePageEl.show();
-            rulesPageEl.hide();
-            lbPageEl.hide();
-            quizPageEl.hide();
+            homePageEl.style.display = "block";
+            rulesPageEl.style.display = "none";
+            lbPageEl.style.display = "none";
+            quizPageEl.style.display = "none";
+            homePageEl.style.display = "flex";
         }
     });
     // if the 'start quiz' button is clicked, the quiz page is shown and hides other pages
-    rulesStartBtnEl.on("click", function() {
+    rulesStartBtnEl.addEventListener("click", function() {
         timeStart();
-
+        gameStart();
         let quizHide = true;
 
         if (quizHide === true) {
-            homePageEl.hide();
-            rulesPageEl.hide();
-            lbPageEl.hide();
-            quizPageEl.show();
+            homePageEl.style.display = "none";
+            rulesPageEl.style.display = "none";
+            lbPageEl.style.display = "none";
+            quizPageEl.style.display = "block";
         }
-        questionChange();
+       
     });
 
 // Leaderboard Page Button
     // if back button is clicked, the home page is shown and hides other pages
-    lbHomeBtnEl.on("click", function() {
+    lbHomeBtnEl.addEventListener("click", function() {
         let homeHide = true;
 
         if (homeHide === true) {
-            homePageEl.show();
-            rulesPageEl.hide();
-            lbPageEl.hide();
-            quizPageEl.hide();
+            homePageEl.style.display = "block";
+            rulesPageEl.style.display = "none";
+            lbPageEl.style.display = "none";
+            quizPageEl.style.display = "none";
+            homePageEl.style.display = "flex";
         }
     });
 
 // Form Page Button
     // if home page button is clicked, the home page is shown and hides other pages
-    formHomeBtnEl.on("click", function() {
+    formHomeBtnEl.addEventListener("click", function() {
         let formHomeHide = true;
 
         if (formHomeHide === true) {
-            homePageEl.show();
-            rulesPageEl.hide();
-            lbPageEl.hide();
-            quizPageEl.hide();
-            formPageEl.hide();
+            homePageEl.style.display = "block";
+            rulesPageEl.style.display = "none";
+            lbPageEl.style.display = "none";
+            quizPageEl.style.display = "none";
+            formPageEl.style.display = "none";
+            homePageEl.style.display = "flex";
         }
+        // reloads the page so the quiz counter and quizzes are reset
+        // location.reload();
     });
     // if leaderboards button is clicked, the home page is shown and hides other pages
-    formLbBtnEl.on("click", function() {
+    formLbBtnEl.addEventListener("click", function() {
         let formLbHide = true;
 
         if (formLbHide === true) {
-            homePageEl.hide();
-            rulesPageEl.hide();
-            lbPageEl.show();
-            quizPageEl.hide();
-            formPageEl.hide();
+            homePageEl.style.display = "block";
+            rulesPageEl.style.display = "none";
+            lbPageEl.style.display = "none";
+            quizPageEl.style.display = "none";
+            formPageEl.style.display = "none";
+            homePageEl.style.display = "flex";
         }
+        // reloads the page so the quiz counter and quizzes are reset
+        // location.reload();
     });
+
+// Questions and Timers
+let questionsCurrent = {};
+let questionsLeft = [];
+let questionCounter = 0;
+let questionTotal = 5;
+
+let displayTime = "";
 
 // Array of Questions and Answers
 let questions = [
     {
         question: "Inside which HTML element do we put the JavaScript?",
-        choiceA1: "<script>",
-        choiceB1: "<scripting>",
-        choiceC1: "<js>",
-        choiceD1: "<javascript>",
-        answer:   "<script>"
+        choice1: "<script>",
+        choice2: "<scripting>",
+        choice3: "<js>",
+        choice4: "<javascript>",
+        answer:   1
     },
     {
         question: "How can you add a comment in a JavaScript?",
-        choiceA1: "<!--This is a comment-->",
-        choiceB1: "'No this is a comment",
-        choiceC1: "// No, no. This is a comment",
-        choiceD1: " ~ Fools, I am the comment. ~",
-        answer:   "// No, no. This is a comment"
+        choice1: "<!--This is a comment-->",
+        choice2: "'No this is a comment",
+        choice3: "// No, no. This is a comment",
+        choice4: " ~ Fools, I am the comment. ~",
+        answer:   3
     },
     {
         question: "How do you call a function named: justFunction?",
-        choiceA1: "call justFunction",
-        choiceB1: "function justFunction()",
-        choiceC1: "justFunction() - Hello?",
-        choiceD1: "justFunction()",
-        answer:   "justFunction()"
+        choice1: "call justFunction",
+        choice2: "function justFunction()",
+        choice3: "justFunction() - Hello?",
+        choice4: "justFunction()",
+        answer:   4
     },
     {
         question: "How do you write a if statement?",
-        choiceA1: "If roses are red and violets are blue, why isn't my coding working?",
-        choiceB1: "if (violet != blue) {}",
-        choiceC1: "if [blue == color] {}",
-        choiceD1: "if {statement} ()",
-        answer:   "if (!violet === blue) {}"
+        choice1: "If roses are red and violets are blue, why isn't my coding working?",
+        choice2: "if (violet != blue) {}",
+        choice3: "if [blue == color] {}",
+        choice4: "if {statement} ()",
+        answer:   2
     },
     {
         question: "Which of these is an array?",
-        choiceA1: "var color = ['I', 'am', 'not', 'an', 'array'];",
-        choiceB1: "let color = ('I', 'am', 'an', 'array');",
-        choiceC1: "const color = {'I', 'am', 'totally', 'an', 'array'};",
-        choiceD1: "array color = ['I', 'am', 'definitely', 'not', 'an', 'array'];",
-        answer:   "var color = ['I', 'am', 'not', 'an', 'array'];"
+        choice1: "var color = ['I', 'am', 'not', 'an', 'array'];",
+        choice2: "let color = ('I', 'am', 'an', 'array');",
+        choice3: "const color = {'I', 'am', 'totally', 'an', 'array'};",
+        choice4: "array color = ['I', 'am', 'definitely', 'not', 'an', 'array'];",
+        answer:   1
     }
 ];
 
-// Starts Time
-let questionCounter = 0;
-let displayTime = "";
-
-answerWrapperEl.on("click", answerCheck);
-
-// Changes the questions and answers randomly from the array
-function questionChange() {
-    questionCounter++;
-    questionEl.text(questions[questionCounter].question);
-    choiceAbtnEl.text(questions[questionCounter].choiceA1);
-    choiceBbtnEl.text(questions[questionCounter].choiceB1);
-    choiceCbtnEl.text(questions[questionCounter].choiceC1);
-    choiceDbtnEl.text(questions[questionCounter].choiceD1);
+function gameStart(){
+    questionCounter = 0;
+    questionsLeft = [...questions];
+        console.log(questionsLeft);
+    questionChange();
 }
 
-function timeStart() {
-    
-    displayTime = 90;
-    questionEl.text(questions[questionCounter].question);
-    choiceAbtnEl.text(questions[questionCounter].choiceA1);
-    choiceBbtnEl.text(questions[questionCounter].choiceB1);
-    choiceCbtnEl.text(questions[questionCounter].choiceC1);
-    choiceDbtnEl.text(questions[questionCounter].choiceD1);
+displayTime = 90;
 
+function timeStart() {
     let timerInterval = setInterval(function() {
         displayTime--;
-        secondsEl.text(displayTime);    
+        secondsEl.textContent = displayTime;    
 
         // if timer hits zero, it will go to the form page
         if(displayTime === 0) {
             clearInterval(timerInterval);
-            // console.log("This works");
-            quizPageEl.hide();
-            formPageEl.show();
-            formScoreEl.text(displayTime);
+            formScoreEl.textContent = displayTime;
+            scoreArr.push(displayTime);
         }
         // if all questions are answered before the timer runs out, it will got to the form page
-        else if (questionCounter > questions.length-1) {
+        else if (questionsLeft.length === 0 || questionCounter >= questionTotal) {
             clearInterval(timerInterval);
-            quizPageEl.hide();
-            setTimeout(function(){
-                formPageEl.show();
-                formScoreEl.text(displayTime);
-            }, 1000);
+            formScoreEl.textContent = displayTime;
+            scoreArr.push(displayTime);
         }
     }, 1000);
 }
-
-let kaomoji = "";
-
-function answerCheck(e) {
-    let userChoice = e.target.id;
-    if (userChoice == "choiceAbtn" || userChoice == "choiceBbtn" || userChoice == "choiceCbtn" || userChoice == "choiceDbtn") {
-        let choiceText = document.getElementById(userChoice).innerText;
-        console.log(choiceText);
-        let answerChoice = (choiceText === questions[questionCounter].answer);
-        if (answerChoice) {
-            kaomoji = document.createElement("p");
-            kaomoji.textContent = "You did it!";
-            quizPageEl.append(kaomoji);
-        }
-        else {
-            kaomoji = document.createElement("p");
-            kaomoji.textContent = "Wrong Answer!";
-            quizPageEl.append(kaomoji);
-            displayTime.textContent = displayTime - 10;
-        }
-        questionChange();
+// Changes the questions and answers randomly from the array
+function questionChange() {
+    
+    if(questionsLeft.length === 0 || questionCounter >= questionTotal) {
+        //  go to form page
+        quizPageEl.style.display = "none";
+        formPageEl.style.display = "block";
     }
+
+    questionCounter++;
+    let questionIndex = Math.floor(Math.random() * questionsLeft.length);
+    questionsCurrent = questionsLeft[questionIndex];
+    questionEl.innerText = questionsCurrent.question;
+
+    choicesEl.forEach( choice => {
+        let number = choice.dataset["number"];
+        choice.innerText = questionsCurrent["choice" + number];
+    });
+
+    questionsLeft.splice(questionIndex, 1);
 }
+
+let choiceText = "";
+
+// Checks Answer
+choicesEl.forEach( choice => {
+    choice.addEventListener("click", event => {
+        // console.log(event.target);
+        
+        let choiceSelect = event.target;
+        let choiceAnswer = choiceSelect.dataset["number"];
+       
+        let choiceCorrect = choiceAnswer == questionsCurrent.answer ? "correct" : "incorrect";
+        
+        choiceSelect.parentElement.classList.add(choiceCorrect);
+
+        setTimeout(function() {
+            choiceSelect.parentElement.classList.remove(choiceCorrect);
+            displayTime = displayTime - 20;
+            questionChange();
+        }, 1000);
+
+        // if (choiceAnswer == questionsCurrent.answer) {
+        // 
+        // }
+        // else {
+        //     displayTime = displayTime - 20;
+        // }
+        // console.log(choiceCorrect);
+        
+    })
+});
+
+function choiceTextRemove() {
+    if(questionCounter != 0){
+        quizPageEl.children[(questionCounter - 1)].setAttribute("style", "display:none");
+    } 
+}
+
